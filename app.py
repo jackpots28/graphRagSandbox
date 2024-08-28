a74d68ca7a778ee0d42c9693591bb07fb87e5fd2
jackpots28 <xblackjack28x@gmail.com>
2024-08-28 15:15:32 -0500

    Update to layout and functionality

--- app.py
+++ app.py
@@ -1,8 +1,49 @@
-from flask import Flask, request, redirect, url_for, send_from_directory, render_template
+import os
+from pathlib import Path
+
+from flask import Flask, flash, request, redirect, url_for, send_from_directory, render_template
+from werkzeug.utils import secure_filename
+import tempfile
+import mammoth
+
+docx_path = Path("/Volumes/stuff/graphRagSandbox/assets/SmartBear_TOU-10FEB2023.docx")
+non_logging_output_dir_path = Path("/Volumes/stuff/graphRagSandbox/templates")
+
+out_temp_file = tempfile.NamedTemporaryFile(prefix="SmartBear_",
+                                            suffix=".html",
+                                            delete=False,
+                                            dir=non_logging_output_dir_path)
+
+with open(docx_path, "rb") as docx_file:
+    result = mammoth.convert_to_html(docx_file)
+    html = result.value
+    with open(out_temp_file.name, "wb") as out_file:
+        print(html.encode("ascii", errors="ignore"))
+        out_file.write(html.encode("ascii", "ignore"))
+
+
+UPLOAD_FOLDER = 'static'
+ALLOWED_EXTENSIONS = {'docx'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        uploaded_file.save(f"static/{uploaded_file.filename}")
+    return redirect(url_for('index'))
 
 
 @app.route("/")
 def index():
-    return render_template("root_frame.html")
+    return render_template("root_frame.html",
+                           static_file_name="SmartBear_5bn3lv8b.html",
+                           dynamic_header="DYNAMIC_HEADER_VALUE")
