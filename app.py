a91f9d8ca5284691f1b69ed624dbaed5dae386ae
jackpots28 <xblackjack28x@gmail.com>
2024-08-30 08:37:57 -0500

    Mid morning commit - got empty submit working and dynamic upload for iframe good

--- app.py
+++ app.py
@@ -1,50 +1,67 @@
-import os
-from pathlib import Path
-
 from flask import Flask, flash, request, redirect, url_for, send_from_directory, render_template
 from werkzeug.utils import secure_filename
+from pathlib import Path
+
 import tempfile
 import mammoth
-
-docx_path = Path("/Volumes/stuff/graphRagSandbox/assets/SmartBear_TOU-10FEB2023.docx")
-non_logging_output_dir_path = Path("/Volumes/stuff/graphRagSandbox/static")
-
-out_temp_file = tempfile.NamedTemporaryFile(prefix="SmartBear_",
-                                            suffix=".html",
-                                            delete=False,
-                                            dir=non_logging_output_dir_path)
+import os
 
 
-with open(docx_path, "rb") as docx_file:
-    result = mammoth.convert_to_html(docx_file)
-    html = result.value
-    with open(out_temp_file.name, "wb") as out_file:
-        print(html.encode("ascii", errors="ignore"))
-        out_file.write(html.encode("ascii", "ignore"))
+# Cleanup file names to be more 'UNIX' like
+def space_underscore_replace(s: str) -> str:
+    return s.replace(" ", "_")
+
+
+# Produce an html converted tempfile that returns just the root path name
+def docx_to_html(i_file: Path, o_dir: Path) -> Path:
+    basename: str = i_file.stem
+    o_tmp_file = tempfile.NamedTemporaryFile(prefix=space_underscore_replace(basename),
+                                             suffix=".html",
+                                             delete=False,
+                                             dir=o_dir)
+
+    with open(i_file, "rb") as docx_file:
+        o_html = mammoth.convert_to_html(docx_file).value
+        with open(o_tmp_file.name, "wb") as html_file:
+            # This was needed to clean up any unicode that was showing up inside docx e.g. euro signs
+            html_file.write(o_html.encode("ascii", "ignore"))
+
+    return Path(o_tmp_file.name)
 
 
 UPLOAD_FOLDER = 'static'
-ALLOWED_EXTENSIONS = {'docx'}
+ALLOWED_EXTENSIONS = {"doc", "docx"}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+def allowed_file(filename) -> bool:
+    allowed_file_output = "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+    return allowed_file_output
 
 
 @app.route('/', methods=['POST'])
 def upload_file():
     uploaded_file = request.files['file']
-    if uploaded_file.filename != '':
+    if uploaded_file.filename != '' and allowed_file(uploaded_file.filename):
         uploaded_file.save(f"static/{uploaded_file.filename}")
-    return render_template("root_frame.html",
-                           static_file_name=f"{Path(out_temp_file.name).name}",
-                           dynamic_header=f"{uploaded_file.filename}")
+        new_html_file = docx_to_html(Path(f"{UPLOAD_FOLDER}/{uploaded_file.filename}"), Path(UPLOAD_FOLDER))
+        return render_template("root_frame.html",
+                               static_file_name=f"{Path(new_html_file).name}",
+                               dynamic_header=f"{uploaded_file.filename}")
+    # TODO - need to keep new tempfiles from being created on resubmit and just serve prior content
+    # elif os.path.exists(f"static/{uploaded_file.filename}"):
+    #     return render_template("root_frame.html",
+    #                            static_file_name=f"{Path(new_html_file).name}",
+    #                            dynamic_header=f"{uploaded_file.filename}")
+    else:
+        return render_template("root_frame.html",
+                               static_file_name="blank.html",
+                               dynamic_header="DYNAMIC_HEADER_VALUE")
 
 
+# Start page - iframe is 'empty'
 @app.route("/")
 def index():
     return render_template("root_frame.html",
